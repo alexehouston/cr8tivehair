95c5f24fc9bb0ed45f4f9b0d8ca6ac6dd0b930af
ahouston <alexehouston@gmail.com>
2023-08-31 12:32:32 -0500

    update mobile nav

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
   return (
     <nav className="Nav navbar fixed-top bg-beige animate__animated animate__fadeInDown animate__slow">
       <div className="container-fluid col-11">
-        <div className="col-9 col-md-3 col-lg-4 d-flex justify-content-start text-uppercase text-center py-3">
+        <div className="col-3 col-md-3 col-lg-4 d-flex justify-content-start text-uppercase text-center py-3">
           <Link to="/">
             <span className="logo head-font fs-3 lh-1">
               Cr<span className="fw-bold">8</span>tive Elements
@@ -16,7 +16,7 @@ export default function Nav() {
             <span className="sub-logo text-xs">Hair Studio</span>
           </Link>
         </div>
-        <div className="nav-links col-md-4 col-3 d-flex justify-content-between">
+        <div className="nav-links col-7 col-md-4 col-3 d-flex justify-content-between">
           <Link className="underline-animation" to="/services">
             Services
           </Link>
